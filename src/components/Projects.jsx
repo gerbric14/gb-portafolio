1816196da94926e6e3818512b61fc6e23bac4397
German Briceno <gerbric14@gmail.com>
2024-03-10 01:02:56 -0500

    Agg gradientes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,10 @@ import { CardImg } from "./CardImg";
 
 export const Projects = () => (
   <section id="proyectos" className={`flex flex-1 flex-col ${styles.marginY}`}>
-    {/* Grandientes
-    <div className="absolute z-[0] w-[50%] h-[50%] right-[5%] rounded-full blue__gradient bottom-50" /> */}
+    {/* Grandientes */}
+    <div className="absolute z-[0] w-[50%] h-[50%] -right-[45%] rounded-full blue__gradient bottom-50" />
+    <div className="absolute z-[0] w-[40%] h-[40%] -right-[35%] rounded-full white__gradient bottom-50" />
+    <div className="absolute z-[0] w-[30%] h-[30%] -right-[30%] rounded-full pink__gradient bottom-50" />
 
     <div className="">
       <h2 className={`${styles.heading2}`}>Proyectos</h2>
@@ -15,13 +17,6 @@ export const Projects = () => (
         ducimus quod culpa dicta? Dolores velit atque quidem quam asperiores.
       </p>
     </div>
-
-    {/* <div className="flex flex-col items-center justify-items-stretch sm:grid sm:grid-cols-2 gap-8 lg:grid-cols-3">
-      <Card />
-      <Card />
-      <Card />
-      
-    </div> */}
 
     <div
       className={`flex flex-col items-center gap-4 md:flex-row md:justify-center text-justify`}
